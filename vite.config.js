7eb3472add72687352f4d6400e5e81ac4fc0a328
Christopher <christopher.ferraro34@gmail.com>
2024-06-05 04:31:46 -0400

    stuff added

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/React-Portfolio/',
   plugins: [react()],
   server: {
     port: 3000,
     open: true,
   },
-  build: {
-    outDir: 'dist', // Specify your output directory
-    rollupOptions: {
-      input: {
-        main: './src/main.jsx' // Adjust the path to your main entry file
-      }
-    }
-  }
 });
